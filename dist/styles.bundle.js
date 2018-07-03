d3b995c530644da5bca368eee96d051bdbf4d11d
Jason James <jasonajames@live.com>
2018-07-03 13:10:28 -0700

    changed bootswatch theme to LUX

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -3,7 +3,7 @@ webpackJsonp(["styles"],{
 /***/ "./node_modules/raw-loader/index.js!./node_modules/postcss-loader/lib/index.js??embedded!./src/styles.css":
 /***/ (function(module, exports) {
 
-module.exports = "@import url('https://bootswatch.com/4/cyborg/bootstrap.min.css');"
+module.exports = "\n@import url('https://bootswatch.com/4/lux/bootstrap.min.css');\n/* @import url('https://bootswatch.com/4/cyborg/bootstrap.min.css'); */ \n"
 
 /***/ }),
 
