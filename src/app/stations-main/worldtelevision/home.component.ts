ba64ea2a6fd278499f253cf01f481cc276af813d
Jason James <jasonajames@live.com>
2018-08-23 08:12:31 -0700

    added Hurricane Lane Feed to Home view

--- src/app/stations-main/worldtelevision/home.component.ts
+++ src/app/stations-main/worldtelevision/home.component.ts
@@ -14,6 +14,8 @@ export class HomeComponent implements OnInit {
   }
 
 stationName = 'World Television';
-description = "Live Video: Kilauea Lava Flow Activity In Lower Puna Hawaii"
+showtitle = "Hurricane Lane heading towards the Hawaiian islands";
+desc = "Hurricane Lane has weakened slightly, but remains a Category 4 storm very close to the islands.";
+footer = " For More... LIKE, FOLLOW & SHARE @SoCalTelevision on Facebook, Instagram & Twitter and watch www.WorldTelevision.tv";
 
 }
