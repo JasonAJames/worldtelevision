fa48de21eb8f0f68f62fdc0d098be2a408d62c46
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-05-21 08:24:22 -0700

    revised Right Now TV feed

--- src/app/cable-channels/right-now-tv/right-now-tv.component.ts
+++ src/app/cable-channels/right-now-tv/right-now-tv.component.ts
@@ -11,6 +11,6 @@ export class RightNowTvComponent implements OnInit {
 
   ngOnInit() {
   }
-  stationName = "Right Now TV"
+  stationName = "RightNow TV"
   description = "Right Now TV is a sports and entertainment television channel headquartered in Atlanta, Georgia. It broadcasts various programming with a mixture of combat sports (wrestling, boxing, MMA), music (music videos, concerts), talk shows, health, fitness, automotive programs, documentaries, and educational & informational children's programs."
 }
