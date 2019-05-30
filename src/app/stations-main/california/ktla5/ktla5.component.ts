49436db9c1d58530463a57523daff757ecbf6aff
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-05-30 07:58:58 -0700

    updated ktla5 feed iframe

--- src/app/stations-main/california/ktla5/ktla5.component.ts
+++ src/app/stations-main/california/ktla5/ktla5.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ktla5',
   templateUrl: './ktla5.component.html',
-  styleUrls: ['./ktla5.component.css', '../../../app.component.css']
+  styleUrls: ['./ktla5.component.css']
 })
 export class Ktla5Component implements OnInit {
 
