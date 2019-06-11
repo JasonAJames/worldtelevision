5e6e0fb241db5042342532c899120208538b0a54
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-06-11 11:21:07 -0700

    updated app component css link path in for10 pheonix stations-main component

--- src/app/components/videofeeds/arizona/fox10-pheonix/fox10-pheonix.component.ts
+++ src/app/components/videofeeds/arizona/fox10-pheonix/fox10-pheonix.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-video-fox10-pheonix',
   templateUrl: './fox10-pheonix.component.html',
-  styleUrls: ['./fox10-pheonix.component.css','../../../../../app.component.css']
+  styleUrls: ['./fox10-pheonix.component.css']
 })
 export class Fox10PheonixComponent implements OnInit {
 
