c70727c1539a008930f4dedef15bb0fb2970acfb
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-06-12 10:55:22 -0700

    added alabama fox6-wbrc app component styling to styles list

--- src/app/stations-main/alabama/fox6-wbrc/fox6-wbrc.component.ts
+++ src/app/stations-main/alabama/fox6-wbrc/fox6-wbrc.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fox6-wbrc',
   templateUrl: './fox6-wbrc.component.html',
-  styleUrls: ['./fox6-wbrc.component.css']
+  styleUrls: ['./fox6-wbrc.component.css','../../../app.component.css']
 })
 export class Fox6WbrcComponent implements OnInit {
 
